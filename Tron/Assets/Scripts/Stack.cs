5f491f00ba5d12b827f5006ea08c7a5c86980e37
fabiola-alston <f.alston.1@estudiantec.cr>
2024-09-13 03:41:20 -0600

    Deleted unnecessary items, created stack class

---
+++ Tron/Assets/Scripts/Stack.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LinkedListNS;
+
+namespace StackNS
+{
+    internal class DoubleNode<T>
+    {
+        public T value;
+        public DoubleNode<T> next;
+        public DoubleNode<T> previous;
+    }
+
+    public class  Stack<T>
+    {
+        private DoubleNode<T> first;
+        private DoubleNode<T> top;
+
+        public void Push(T value)
+        {
+            DoubleNode<T> newItem = new DoubleNode<T>();
+            newItem.value = value;
+
+            if (first == null)
+            {
+                first = newItem;
+                top = newItem;
+            }
+            else
+            {
+                DoubleNode<T> current = first;
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = newItem;
+                newItem.previous = current;
+                top = newItem;
+            }
+        }
+
+        public void Pop()
+        {
+            if (first == null)
+            {
+                throw new System.Exception("Stack is empty");
+            }
+            else if (top == first)
+            {
+                first = top = null;
+            }
+            else
+            {
+                top = top.previous;
+                top.next.previous = null;
+                top.next = null;
+            }
+            
+        }
+
+        public T Top()
+        {
+            return top.value;
+        }
+    }
+}
